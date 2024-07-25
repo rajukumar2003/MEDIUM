1da66bf2ecb893b867228214bfa66a33c4d06f4b
Raju kumar <rajukumarsingh12003@gmail.com>
2024-07-25 17:30:20 +0530

    added vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { Blog } from './pages/Blog'
 import { Blogs } from './pages/Blogs'
 import { Publish } from './pages/Publish'
 import { LandingPage } from './pages/LandingPage'
+import { Analytics } from '@vercel/analytics/react'
 
 
 function App() {
@@ -20,7 +21,8 @@ function App() {
           <Route path='/blog' element={<Blog />} />
           <Route path='/blogs' element={<Blogs />} />
           <Route path="/blog/:id" element={<Blog />} />
-          <Route path="/publish" element={<Publish/>} />
+          <Route path="/publish" element={<Publish />} />
+          <Analytics/>
         </Routes>
       </BrowserRouter>
     </>
